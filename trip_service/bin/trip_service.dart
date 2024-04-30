36e593ee878f54b4dc2291e87fa09e0c938fc781
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-05-01 02:29:37 +0500

    Trip service kata added

---
+++ trip_service/bin/trip_service.dart
@@ -0,0 +1,5 @@
+import 'package:trip_service/trip_service.dart' as trip_service;
+
+void main(List<String> arguments) {
+  print('Hello world: ${trip_service.calculate()}!');
+}
---
+++ trip_service/test/trip_service_test.dart
@@ -0,0 +1,8 @@
+import 'package:trip_service/trip_service.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
