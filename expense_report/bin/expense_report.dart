02d4ff34d0835ffa7865c5ceb846e674b95581cc
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-05-01 22:14:31 +0500

    Expense report kata added

---
+++ expense_report/bin/expense_report.dart
@@ -0,0 +1,5 @@
+import 'package:expense_report/expense_report.dart' as expense_report;
+
+void main(List<String> arguments) {
+  print('Hello world: ${expense_report.calculate()}!');
+}
---
+++ expense_report/test/expense_report_test.dart
@@ -0,0 +1,8 @@
+import 'package:expense_report/expense_report.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
