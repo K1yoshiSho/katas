a2c19c486e3df908fcbc87a587edd499f109ae2c
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-05-14 00:58:43 +0500

    . r duplicate loops

--- expense_report/lib/src/expense_report.dart
+++ expense_report/lib/src/expense_report.dart
@@ -10,31 +10,22 @@ class ExpenseReport {
 
     log('Expense Report: $date');
     for (final expense in expenses) {
-      processExpense(expense, (meal, total) {
-        mealExpenses += meal;
-        totalExpenses += total;
-      });
+      if (expense.type == ExpenseType.dinner || expense.type == ExpenseType.breakfast) {
+        mealExpenses += expense.amount;
+      }
+    }
+    for (final expense in expenses) {
+      writeExpenseLineFor(expense);
+      totalExpenses += expense.amount;
     }
     log('Meal Expenses: $mealExpenses');
     log('Total Expenses: $totalExpenses');
   }
 
-  void processExpense(Expense expense, void Function(int, int) updateExpenses) {
-    var mealAmount = 0;
-    var totalAmount = 0;
-
-    if (expense.type == ExpenseType.dinner || expense.type == ExpenseType.breakfast) {
-      mealAmount = expense.amount;
-    }
-
-    log('${expense.type.name}\t${expense.amount}\t${isOverExpensed(expense) ? 'X' : ' '}');
-    totalAmount = expense.amount;
-
-    updateExpenses(mealAmount, totalAmount);
-  }
+  void writeExpenseLineFor(Expense expense) => log('${expense.type.name}\t${expense.amount}\t${isOverExpensed(expense) ? 'X' : ' '}');
 
   bool isOverExpensed(Expense expense) =>
-      (expense.type == ExpenseType.dinner && expense.amount > 5000) || (expense.type == ExpenseType.breakfast && expense.amount > 1000);
+      expense.type == ExpenseType.dinner && expense.amount > 5000 || expense.type == ExpenseType.breakfast && expense.amount > 1000;
 
   void log(Object? object) => ApprovalLogger.log(object.toString());
 }
